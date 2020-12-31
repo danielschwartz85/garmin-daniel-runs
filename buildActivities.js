7ab9f06e5cef9adf9f3570beccbfec9a74dcfad9
Daniel Schwartz <danielsch24@gmail.com>
2020-12-31 19:45:18 +0200

    added builActivities.js

---
+++ buildActivities.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+
+require('dotenv').config();
+const garminRunFetch = require('garmin-run-fetch');
+const fs = require('fs');
+const util = require('util');
+
+const pWriteFile = util.promisify(fs.writeFile);
+
+const opts = {
+  userName: process.env.GARMIN_USER_NAME,
+  password: process.env.GARMIN_PASSWORD,
+  // limit,
+  // startDate,
+  // endDate,
+};
+
+(async function buildActivities() {
+  console.log('getting activities');
+  const activitiesJson = await garminRunFetch(opts);
+  const activities = JSON.stringify(activitiesJson);
+
+  console.log('writing activities');
+  pWriteFile('./activities.js', `window.activities = ${activities}`);
+
+  console.log('build successfull');
+}());
