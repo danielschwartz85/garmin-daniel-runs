1ba3056e80c45fb18c9a8ce8edc1c06730c4d4fd
Daniel Schwartz <danielsch24@gmail.com>
2021-01-25 00:39:02 +0200

    cleaning

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"MyRuns","short_name":"MyRuns","start_url":"/","background_color":"#6b37bf","theme_color":"#6b37bf","display":"standalone","icon":"src/images/run.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"20558f68f558bb044afb20e4d9e20aac"},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
