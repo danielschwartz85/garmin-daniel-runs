24c0345cfc6b0e3691ca9713feb64a03b82c4704
Daniel Schwartz <danielsch24@gmail.com>
2021-01-25 19:05:52 +0200

    updated at color chaneg

--- src/components/updatedAt.js
+++ src/components/updatedAt.js
@@ -9,7 +9,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     textAlign: 'center',
     margin: '10px',
-    color: 'firebrick',
+    color: 'rgba(0, 0, 0, 0.87)',
   },
 }));
 
