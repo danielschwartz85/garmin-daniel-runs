6d9f51ff0a0fc9e157d4342ea603cf2e304de8f3
Daniel Schwartz <danielsch24@gmail.com>
2021-01-25 19:06:28 +0200

    component name change

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import SEO from '../components/seo';
 import Table from '../components/table';
 import UpdatedAt from '../components/updatedAt';
 
-export default function MyRun() {
+export default function MyRuns() {
   return (
     <div>
       <SEO />
