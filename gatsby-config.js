3fad969d0de21bf6b08c7254f693b1710259b535
Daniel Schwartz <danielsch24@gmail.com>
2021-01-29 15:29:15 +0200

    changed mnifest icon and color

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,12 +31,12 @@ module.exports = {
         name: 'MyRuns',
         short_name: 'MyRuns',
         start_url: '/',
-        background_color: '#6b37bf', // TODO
-        theme_color: '#6b37bf', // TODO
+        background_color: '#fad28e',
+        theme_color: '#fad28e',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icon: 'src/images/run.png', // This path is relative to the root of the site.
+        icon: 'src/images/tracking.png', // This path is relative to the root of the site.
       },
     },
     // Create service worker (proxy between network and page for better offline capabilities)
