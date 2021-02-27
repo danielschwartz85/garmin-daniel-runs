47df6b056a89a4ddb0b1cb8c8bf3cd6b3b9929e1
Daniel Schwartz <danielsch24@gmail.com>
2021-02-27 19:13:36 +0200

    fix: fixed 402 exception and exiting on errors

--- src/scripts/fetchActivities.js
+++ src/scripts/fetchActivities.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 const garminRunFetch = require('garmin-run-fetch');
 const fs = require('fs');
 const util = require('util');
-
-const pWriteFile = util.promisify(fs.writeFile);
+const { exit } = require('process');
 
 const opts = {
   userName: process.env.GARMIN_USER_NAME,
@@ -15,10 +14,17 @@ const opts = {
   // endDate,
 };
 
+const pWriteFile = util.promisify(fs.writeFile);
+const pResult = (promise) => promise.then((r) => [undefined, r]).catch((e) => [e]);
+
 (async function buildActivities() {
   console.log('getting activities');
-  const activitiesJson = await garminRunFetch(opts);
-  const activities = JSON.stringify(activitiesJson);
+  const [err, activitiesJson] = await pResult(garminRunFetch(opts));
+  const activities = !err && JSON.stringify(activitiesJson);
+  if (err) {
+    console.error('Failed to fetch data', err);
+    exit(1);
+  }
 
   console.log('updating activities');
   pWriteFile(`${__dirname}/../data/activities.json`, activities);
